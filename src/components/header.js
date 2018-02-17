3af0c84f840e1b5118dbe0303bb309ea50470063
Justin Bond <bond.justink@gmail.com>
2018-02-16 21:31:22 -0800

    Header :: added menu active state

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,25 @@ import logo from '../images/jb-logo.svg';
 export default class header extends Component {
 	constructor(props) {
 		super();
-
+		this.handleClick = this.handleClick.bind(this);
+		this.state = {
+			menuActive: false
+		}
+	}
+	handleClick() {
+		if (!this.state.menuActive) {
+			this.setState({
+				menuActive: true
+			});
+		} else {
+			this.setState({
+				menuActive: false
+			});
+		}
 	}
 	render() {
 		const scrolled = this.props.scrollPosition > 200 ? 'scrolled' : '';
+		const menuActive = this.state.menuActive ? 'active' : '';
 		console.log(this.props.scrollPosition);
 		console.log(scrolled);
 		return (
@@ -18,7 +33,7 @@ export default class header extends Component {
 						<img src={logo} className="header__logo col-xs-1" alt="logo" />
 					</Link>
 				</div>
-				<div id="site-menu" className={`site-menu ${scrolled}`}>
+				<div id="site-menu" className={`site-menu ${scrolled} ${menuActive}`}>
 					<div className="site-menu__wrapper">
 						<nav className="site-menu__navigation">
 							<ul>
@@ -28,9 +43,9 @@ export default class header extends Component {
 							</ul>
 						</nav>
 					</div>
-					<div className="site-menu__control">
+					<div className="site-menu__control" onClick={this.handleClick}>
 						<div className="site-menu__menu-button">
-							<div className="hamburger"></div>
+							<img src="http://dummyimage.com/65x25/4d494d/686a82.gif&text=placeholder+image" alt="site-menu" />
 						</div>
 					</div>
 				</div>
