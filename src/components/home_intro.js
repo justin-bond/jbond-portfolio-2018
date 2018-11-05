ea91faf36a241e46f48bc3bd1d3d47fb40b22e9c
Justin Bond <bond.justink@gmail.com>
2018-11-04 18:25:31 -0800

    :bug: fix for button focus, update intro

--- src/components/home_intro.js
+++ src/components/home_intro.js
@@ -5,11 +5,11 @@ export default class home_intro extends Component {
 		return (
 			<div className="home-intro__container">
 				<h1 className="home-intro__text">
-					Hello, I'm Justin Bond and I am a full-stack web developer from Costa Mesa, California.
+					Hello, I'm Justin Bond and I am a full-stack web developer in Orange County, California.
 				</h1>
 				<div className="home-intro__bullets">
 					<ul>
-						<li>Skilled in Wordpress, Magento, PHP, and Javascript.</li>
+						<li>Skilled in Wordpress, Magento, Shopify, PHP, and Javascript.</li>
 						<li>When I am not coding I enjoy listening to music, watching football, golfing, riding motorcycles, and going on hikes with my pup.</li>
 					</ul>
 				</div>
